0e395751cf1fead4dd7bf9ac78b8d4b0833ffde7
Rahul Sinha <rahulkumarsinha1234@gmail.com>
2021-09-16 20:35:33 +0530

    nnn

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to Reload SNEHA ----------------SINHA
+          Edit <code>src/App.js</code> and save to Reload Nirbhay ----------------SINHA
         </p>
         <a
           className="App-link"
