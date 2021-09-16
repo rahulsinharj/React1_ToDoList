352c3e1c56a21d89af0cc1022e5f6158002f9429
Rahul Sinha <rahulkumarsinha1234@gmail.com>
2021-09-16 20:42:09 +0530

    lower the space

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to Reload Nirbhay ----------------SINHA
+          Edit <code>src/App.js</code> and save to Reload Nirbhay -- SINHA
         </p>
         <a
           className="App-link"
